5031555a1396bbaecd8aa5896f8edad982d6c9e8
Andrey Semenov <andss82@gmail.com>
2020-10-02 09:11:21 +0300

    task 2 done

--- src/js/app.js
+++ src/js/app.js
@@ -4,5 +4,7 @@ import Character from './character';
 // eslint-disable-next-line no-console
 console.log('worked');
 
+const character = new Character('Alex', 'Bowman');
+Character.prototype.damage.call(character, 100);
 // eslint-disable-next-line no-console
-console.log(Character('alex', 'Magician'));
+console.log(character);
